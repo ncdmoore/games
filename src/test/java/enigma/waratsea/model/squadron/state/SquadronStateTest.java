11351963d76b2002c766510055a7e8593de1b048
ncdmoore <ncdmoore@gmail.com>
2019-10-16 23:21:56 -0400

    Resource cleanup.

--- src/test/java/enigma/waratsea/model/squadron/state/SquadronStateTest.java
+++ src/test/java/enigma/waratsea/model/squadron/state/SquadronStateTest.java
@@ -13,7 +13,7 @@ public class SquadronStateTest {
     public void readyTransistionTest() {
         SquadronState squadronState = READY;
 
-        squadronState = squadronState.transistion(SquadronAction.ASSIGN_TO_PATROL);
+        squadronState = squadronState.transition(SquadronAction.ASSIGN_TO_PATROL);
 
         Assert.assertSame(squadronState, QUEUED_FOR_PATROL);
     }
@@ -22,7 +22,7 @@ public class SquadronStateTest {
     public void readyInvalidTransitionTest() {
         SquadronState squadronState = READY;
 
-        squadronState = squadronState.transistion(SquadronAction.RETURN);
+        squadronState = squadronState.transition(SquadronAction.RETURN);
 
         Assert.assertSame(squadronState, READY);
     }
@@ -31,7 +31,7 @@ public class SquadronStateTest {
     public void queuedForPatrolTest() {
         SquadronState squadronState = QUEUED_FOR_PATROL;
 
-        squadronState = squadronState.transistion(SquadronAction.REMOVE_FROM_PATROL);
+        squadronState = squadronState.transition(SquadronAction.REMOVE_FROM_PATROL);
 
         Assert.assertSame(squadronState, READY);
     }
@@ -40,7 +40,7 @@ public class SquadronStateTest {
     public void queuedForMissionTest() {
         SquadronState squadronState = QUEUED_FOR_MISSION;
 
-        squadronState = squadronState.transistion(SquadronAction.REMOVE_FROM_MISSION);
+        squadronState = squadronState.transition(SquadronAction.REMOVE_FROM_MISSION);
 
         Assert.assertSame(squadronState, READY);
     }
@@ -49,7 +49,7 @@ public class SquadronStateTest {
     public void onPatrolTransitionTest() {
         SquadronState squadronState = ON_PATROL;
 
-        squadronState = squadronState.transistion(SquadronAction.REMOVE_FROM_PATROL);
+        squadronState = squadronState.transition(SquadronAction.REMOVE_FROM_PATROL);
 
         Assert.assertSame(squadronState, HANGER);
     }
@@ -58,7 +58,7 @@ public class SquadronStateTest {
     public void onMissionInvalidTransitionTest() {
         SquadronState squadronState = ON_MISSION;
 
-        squadronState = squadronState.transistion(SquadronAction.ASSIGN_TO_PATROL);
+        squadronState = squadronState.transition(SquadronAction.ASSIGN_TO_PATROL);
 
         Assert.assertSame(squadronState, ON_MISSION);
     }
@@ -67,7 +67,7 @@ public class SquadronStateTest {
     public void inHangerTransitionTest() {
         SquadronState squadronState = HANGER;
 
-        squadronState = squadronState.transistion(SquadronAction.REFIT);
+        squadronState = squadronState.transition(SquadronAction.REFIT);
 
         Assert.assertSame(squadronState, READY);
     }
