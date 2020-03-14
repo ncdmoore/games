d7865bc5e6494e313c1721eff26a36d318dbff97
ncdmoore <ncdmoore@gmail.com>
2020-03-13 21:42:20 -0400

    Support alternate values for mission role serialization.

--- src/main/java/engima/waratsea/model/base/airfield/mission/MissionRole.java
+++ src/main/java/engima/waratsea/model/base/airfield/mission/MissionRole.java
@@ -1,9 +1,13 @@
 package engima.waratsea.model.base.airfield.mission;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 
 public enum MissionRole {
+    @SerializedName(value = "MAIN", alternate = {"Main", "main"})
     MAIN("Main"),
+
+    @SerializedName(value = "ESCORT", alternate = {"Escort", "escort"})
     ESCORT("Escort");
 
     @Getter
@@ -16,5 +20,15 @@ public enum MissionRole {
      */
     MissionRole(final String value) {
         this.value = value;
+    }
+
+    /**
+     * The String representation of this enum.
+     *
+     * @return The String representation of this enum.
+     */
+    @Override
+    public String toString() {
+        return value;
     }
 }
