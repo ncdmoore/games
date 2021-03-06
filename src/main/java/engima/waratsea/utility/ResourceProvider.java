d139fd0b5969e6fffa68597398c853a80f22d4c3
David Moore <david.moore@sas.com>
2021-03-06 10:08:58 -0500

    Rename ImageResourceProvider.java to ResourceProvider.java.

--- src/main/java/engima/waratsea/utility/ResourceProvider.java
+++ src/main/java/engima/waratsea/utility/ResourceProvider.java
@@ -8,10 +8,12 @@ import engima.waratsea.model.game.Side;
 import engima.waratsea.model.vessel.Vessel;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Singleton;
 import java.io.File;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Optional;
@@ -22,12 +24,14 @@ import java.util.Optional;
 @Singleton
 @Slf4j
 public class ResourceProvider {
-
     private static final String IMAGE_EXT = ".png";
+    private static final String SOUND_EXT = ".mp3";
     private static final String AIRCRAFT_DIR = "/aircraft/";
     private static final String SCENARIO_DIR = "/scenarios/";
     private static final String SHIPS_DIR = "/ships/";
     private static final String IMAGES_DIR = "images/";
+    private static final String SOUNDS_DIR = "sounds/";
+
     private final GameTitle gameTitle;
     private final Resource resource;
 
@@ -67,8 +71,18 @@ public class ResourceProvider {
         String scenario = resource.getScenario();
         String path = gameTitle.getValue() + SCENARIO_DIR + scenario + "/" + resourceName;
         log.debug("get image: {}", path);
-        Optional<Image> image = loadImage(path);
+        Optional<Image> image = loadImageResource(path);
         return image.orElseGet(() -> getGameImage(resourceName));
+    }
+
+    /**
+     * Get a media.
+     *
+     * @param resourceName The resource file name.
+     * @return The media if it exists.
+     */
+    public Media getMedia(final String resourceName) {
+        return getGameMedia(resourceName);
     }
 
     /**
@@ -86,7 +100,7 @@ public class ResourceProvider {
     public Image getImage(final String scenario, final String resourceName) {
         String path = gameTitle.getValue() + SCENARIO_DIR + scenario + "/" + resourceName;
         log.debug("get image: {}", path);
-        Optional<Image> image = loadImage(path);
+        Optional<Image> image = loadImageResource(path);
         return image.orElseGet(() -> getGameImage(resourceName));
     }
 
@@ -212,7 +226,7 @@ public class ResourceProvider {
     private Optional<Image> getShipNameImage(final Vessel ship) {
         log.debug("look for ship name '{}'", ship.getName());
         String path = gameTitle.getValue() + SHIPS_DIR + ship.getSide() + "/images/" + ship.getName() + IMAGE_EXT;
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     /**
@@ -224,7 +238,7 @@ public class ResourceProvider {
     private Optional<Image> getShipClassImage(final Vessel ship) {
         log.debug("look for ship class '{}'", ship.getShipClass());
         String path = gameTitle.getValue() + SHIPS_DIR + ship.getSide() + "/images/" + ship.getShipClass() + IMAGE_EXT;
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     /**
@@ -236,7 +250,7 @@ public class ResourceProvider {
     private Optional<Image> getShipNameProfileImage(final Vessel ship) {
         log.debug("Look for ship name profile '{}'", ship.getName());
         String path = gameTitle.getValue() + SHIPS_DIR + ship.getSide() + "/images/" + ship.getName() + "-profile.png";
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     /**
@@ -248,7 +262,7 @@ public class ResourceProvider {
     private Optional<Image> getShipClassProfileImage(final Vessel ship) {
         log.debug("Look for ship class profile '{}'", ship.getShipClass());
         String path = gameTitle.getValue() + SHIPS_DIR + ship.getSide() + "/images/" + ship.getShipClass() + "-profile.png";
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     /**
@@ -259,7 +273,7 @@ public class ResourceProvider {
      */
     private Optional<Image> getAircraftImage(final Aircraft aircraft) {
         String path = gameTitle.getValue() + AIRCRAFT_DIR + aircraft.getSide() + "/images/" + aircraft.getModel() + IMAGE_EXT;
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     /**
@@ -271,12 +285,16 @@ public class ResourceProvider {
      */
     private Optional<Image> getAircraftProfileImage(final Side side, final String aircraft) {
         String path = gameTitle.getValue() + AIRCRAFT_DIR + side + "/images/" + aircraft + "-profile.png";
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     private Image getGameImage(final String resourceName) {
         return getGameSpecificImage(resourceName)
                 .orElseGet(() -> getDefaultImage(resourceName));
+    }
+
+    private Media getGameMedia(final String resourceName) {
+        return getDefaultMedia(resourceName);
     }
 
     /**
@@ -287,7 +305,7 @@ public class ResourceProvider {
      */
     private Optional<Image> getGameSpecificImage(final String resourceName) {
         String path = gameTitle.getValue() + "/" + IMAGES_DIR + resourceName;
-        return loadImage(path);
+        return loadImageResource(path);
     }
 
     /**
@@ -299,13 +317,26 @@ public class ResourceProvider {
      */
     private Image getDefaultImage(final String resourceName) {
         String path = IMAGES_DIR + resourceName;
-        Optional<Image> image = loadImage(path);
+        Optional<Image> image = loadImageResource(path);
 
         if (image.isEmpty()) {
-            log.error("Unable to load resource: {}", path);
+            log.error("Unable to load image resource: {}", path);
         }
 
         return image.orElse(null);
+    }
+
+    private Media getDefaultMedia(final String resourceName) {
+        String path = SOUNDS_DIR + resourceName;
+        log.debug("Load media at path: '{}'", path);
+
+        Optional<Media> media = loadMediaResource(path);
+
+        if (media.isEmpty()) {
+            log.error("Unable to load media resource: {}", path);
+        }
+
+        return media.orElse(null);
     }
 
     /**
@@ -314,11 +345,20 @@ public class ResourceProvider {
      * @param path to the image file.
      * @return The image if it exists.
      */
-    private Optional<Image> loadImage(final String path) {
+    private Optional<Image> loadImageResource(final String path) {
         return Optional.ofNullable(getClass()
                 .getClassLoader()
                 .getResource(path))
-                .map(this::getFile);
+                .map(this::getFileURI)
+                .map(this::getImageFromURI);
+    }
+
+    private Optional<Media> loadMediaResource(final String path) {
+        return Optional.ofNullable(getClass()
+                .getClassLoader()
+                .getResource(path))
+                .map(this::getFileURI)
+                .map(this::getMediaFromURI);
     }
 
     /**
@@ -327,16 +367,23 @@ public class ResourceProvider {
      * @param url The image file url.
      * @return The image from the file.
      */
-    private Image getFile(final URL url) {
-
+    private URI getFileURI(final URL url) {
        try {
            File file =  new File(url.toURI().getPath());
            log.debug("Loaded image: {}", url.toURI().getPath());
-           return new Image(file.toURI().toString());
+           return file.toURI();
        } catch (URISyntaxException ex) {
            log.error("Unable to get URI from URL.", ex);
            return null;
        }
+    }
+
+    private Image getImageFromURI(final URI uri) {
+        return new Image(uri.toString());
+    }
+
+    private Media getMediaFromURI(final URI uri) {
+        return new Media(uri.toString());
     }
 
 }
